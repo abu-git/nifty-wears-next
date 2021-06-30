ce3e31d8c09926041060d841f495b2ed9ed921bd
Sam Abu <abu.sammie@gmail.com>
2021-06-30 20:30:21 +0200

    showcase idea 3 progress

--- components/AfterNav.js
+++ components/AfterNav.js
@@ -4,20 +4,39 @@ import { Box } from "@material-ui/core"
 
 
 const useStyles =  makeStyles((theme) => ({
-    landing: {
-        height: '450px',
+    showcase: {
+        minHeight: "60vh",
+        backgroundImage: `url("/nifty-backdrop.jpg")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        color: "white",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        isolation: "isolate",
+        /*alignItems: "center",
         marginTop: "50px",
         /*border: "1px solid red"*/
     },
-    showcasetext: {
+    showcaseIntro: {
         /*font: `"Yellowtail", cursive`,*/
-        textAlign: "center",
+        width: "50%",
+        padding: "2em",
+        paddingTop: "min(15vh, 10rem)",
+        position: "relative",
+        /*textAlign: "center",
         fontSize: "1.8rem",
-        fontStyle: "italic"
-    }
+        fontStyle: "italic"*/
+
+        '&::after' : {
+            content: "",
+            position: "absolute",
+            inset: "0",
+            zIndex: -1,
+            background: "#355f08",
+            opacity: 1,
+            /*mixBlendMode: "difference"*/
+        }
+    },
+    
 }))
 
 export default function AfterNav() {
@@ -26,21 +45,14 @@ export default function AfterNav() {
     return(
         <Container maxWidth="md">
         <Grid container>
-            <Grid item xs={6} className={classes.landing}>
-                <Box className={classes.showcasetext}>30% off on your first buy<h1>Nifty Wears</h1></Box>
-                {/*<Box><p>shop with us</p></Box>*/}
-            </Grid>
-            <Grid item xs={6}>
-            <Box className={classes.landing} paddingTop="40px">
-                <Image 
-                    src="/model.jpg"
-                    alt="Nifty Wears Shop Promo"
-                    width={360}
-                    height={360}
-                />
-            </Box>
-            </Grid>
-            
+            <Grid item lg={12} sm={12} md={12} xs={12}>
+                <div className={classes.showcase}>
+                    <div className={classes.showcaseIntro}>
+                        <p>30% off on your first buy</p><h1>Nifty Wears</h1>
+                    </div>
+                </div>
+                {/*<Box className={classes.showcaseIntro}>30% off on your first buy<h1>Nifty Wears</h1></Box>*/}
+            </Grid>            
         </Grid>
         </Container>
         
