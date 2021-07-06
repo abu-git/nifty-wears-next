efe6c0996b3bb346b8e9ff777f56686a24449986
Sam Abu <abu.sammie@gmail.com>
2021-07-07 00:22:37 +0200

    removed some unused code

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,13 @@
 import headerStyles from '../styles/Navbar.module.css'
-import { makeStyles, IconButton } from "@material-ui/core"
+import { IconButton } from "@material-ui/core"
 import Image from 'next/image'
-import { MicNone } from '@material-ui/icons'
 
-const useStyles = makeStyles((theme) => ({
-    logoimage: {
-        width: "0.5rem",
-        height: "1rem",
-        display: "flex",
-        justifyContent: "center"
-    },
-    
-}))
+
 
 export default function Navbar() {
-    const classes = useStyles()
+   
     return(
         <header className={headerStyles.header}>
-            {/*<div className={headerStyles.logo}>
-                <IconButton>
-                    <Image width="30px" height="30px" alt="Nifty Wears logo"  src="/assets/favicon_io/favicon-32x32.png" />
-                </IconButton>
-                <h3>Nifty Wears</h3>    
-            </div>*/}
             <IconButton className={headerStyles.logo}>
                 <Image width="30px" height="30px" alt="Nifty Wears logo"  src="/assets/favicon_io/favicon-32x32.png" />
                 <h3 className={headerStyles.logotitle}>Nifty Wears</h3>
