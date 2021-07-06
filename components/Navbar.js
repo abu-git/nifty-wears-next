73f9a1a249bd415a774aac2955eaee533428b85f
Sam Abu <abu.sammie@gmail.com>
2021-07-06 21:58:54 +0200

    navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,34 @@
 import headerStyles from '../styles/Navbar.module.css'
 import { makeStyles, IconButton } from "@material-ui/core"
 import Image from 'next/image'
+import { MicNone } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({
     logoimage: {
         width: "0.5rem",
         height: "1rem",
         display: "flex",
-        justifyContent: "center"
-    }
+        justifyContent: "center",
+        /*[theme.breakpoints.down('sm')]: {
+            display: "none"
+        }*/
+    },
+    
 }))
 
 export default function Navbar() {
     const classes = useStyles()
     return(
         <header className={headerStyles.header}>
+            {/*<div className={headerStyles.logo}>
+                <IconButton>
+                    <Image width="30px" height="30px" alt="Nifty Wears logo"  src="/assets/favicon_io/favicon-32x32.png" />
+                </IconButton>
+                <h3>Nifty Wears</h3>    
+            </div>*/}
             <IconButton>
                 <Image width="30px" height="30px" alt="Nifty Wears logo"  src="/assets/favicon_io/favicon-32x32.png" />
             </IconButton>
-            {/*<Image alt="Nifty Wears logo" className={classes.logoimage} layout="fill" src="/assets/favicon_io/favicon-32x32.png" />*/}
             <h3 className={headerStyles.logo}>Nifty Wears</h3>
             <input type="checkbox" id="nav-toggle" className={headerStyles.navtoggle}/>
             <nav className={headerStyles.nav}>
