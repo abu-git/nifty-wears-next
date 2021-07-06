0317fb35387ad9a5b8e7becb32101f5bff0f85c5
Sam Abu <abu.sammie@gmail.com>
2021-07-06 22:02:58 +0200

    navbar blog to shop correction

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,7 +35,7 @@ export default function Navbar() {
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Shop</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
             </nav>
