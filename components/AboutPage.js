7fd29c52f8e680d65b0c5f25ef9dfc3fa68e1a16
Sam Abu <abu.sammie@gmail.com>
2021-07-19 13:06:59 +0200

    about page progress

--- components/AboutPage.js
+++ components/AboutPage.js
@@ -1,14 +1,20 @@
-import  { makeStyles, Container } from '@material-ui/core'
+import Link from 'next/link'
+import  { makeStyles, Container, Grid } from '@material-ui/core'
+import { ArrowBack } from '@material-ui/icons'
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        minHeight: "100vh",
+        minHeight: "70vh",
         backgroundColor: "hsl(0, 100%, 0%)",
         color: "white",
         display: "grid",
         gridTemplateColumns: "50% 50%",
-        gap: "1em"
+        gap: "1em",
+        [theme.breakpoints.down('md')]: {
+            display: "flex",
+            flexDirection: "column"
+        }
     },
     p_code: {
         color: "white"
@@ -17,15 +23,63 @@ const useStyles = makeStyles((theme) => ({
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        textAlign: "center",
         //border: "1px solid red",
-        maxHeight: "90vh"
+        maxHeight: "90vh",
+        [theme.breakpoints.down('md')]: {
+            maxHeight: "30vh",
+        },
+        [theme.breakpoints.down('sm')]: {
+            maxHeight: "20vh",
+            marginTop: "30px"
+            //justifyContent: "center",
+            //alignItems: "center"
+        },
+        [theme.breakpoints.down('lg')]: {
+            maxHeight: "90vh"
+        }
     },
     right: {
         display: "flex",
         textAlign: "center",
-        alignItems: "center",
         //border: "1px solid red",
-        maxHeight: "90vh"
+        maxHeight: "90vh",
+        [theme.breakpoints.down('sm')]:{
+            marginTop: "0px"
+        },
+        [theme.breakpoints.down('md')]: {
+            //maxHeight: "30vh",
+            marginTop: "0px"
+        },
+        [theme.breakpoints.down('lg')]: {
+            maxHeight: "90vh",
+            alignItems: "center",
+            //border: "1px solid red"
+        }
+    },
+    arrowback: {
+        marginTop: "5px",
+        color: "white",
+        width: "1.5em",
+        height: "1.3em",
+        /*"&:hover": {
+            color: "#ffca68"
+        }*/
+    },
+    backtext: {
+        color: "white",
+        paddingBottom: "3px",
+        "&:hover": {
+            //color: "#ffca68",
+            textDecoration: "underline",
+            cursor: "pointer"
+        }
+    },
+    goback: {
+        marginTop: "15px",
+        '&:hover': {
+            textDecoration: "underline"
+        }
     }
 }))
 
@@ -34,18 +88,24 @@ export default function AboutPage(){
     return(
         <>
             <Container maxWidth="xl">
-                <div className={classes.root}>
-                    <div className={classes.left}>
-                        <h1>About Us</h1>
+                <Link href="/">
+                    <div className={classes.goback}>
+                        <ArrowBack className={classes.arrowback} />
+                        <span className={classes.backtext}> Back to home</span>
                     </div>
-                    <div className={classes.right}>
+                </Link>
+                <Grid container className={classes.root}>
+                    <Grid item className={classes.left}>
+                        <h1>About Us</h1>
+                    </Grid>
+                    <Grid item className={classes.right}>
                         <p className={classes.p_code}>
                             We are a start up attire company with a simple business idea.
                             Our aim is to procure and produce suitable clothing for anyone 
                             willing to see value in our branded products.
                         </p>
-                    </div>
-                </div>  
+                    </Grid>
+                </Grid>  
             </Container>
             
         </>
