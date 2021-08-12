8ef645dc26b0a69a4181ad432aec9b77372d8016
abu-git <abu.sammie@gmail.com>
2021-08-12 19:28:12 +0200

    new cart page started

---
+++ components/CartPage3.js
@@ -0,0 +1,17 @@
+import { Container, Grid, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+
+}))
+
+export default function CartPage3() {
+    const classes = useStyles()
+
+    return(
+        <>
+            <Container maxWidth="xl">
+
+            </Container>
+        </>
+    )
+}
