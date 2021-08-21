b3d50b032cca3479dfc18db7924c806ac0f6f4d1
abu-git <abu.sammie@gmail.com>
2021-08-21 18:45:08 +0200

    new cart progress

--- components/CartPage3.js
+++ components/CartPage3.js
@@ -2,6 +2,12 @@ import { Breadcrumbs,Container, Grid, makeStyles, Typography } from "@material-u
 import Link from '@material-ui/core/Link'
 import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 import Meta from "./Meta"
+import Image from 'next/image'
+
+import cartStyles from '../styles/CartPage.module.css'
+
+import { CartContext } from "../context/CartContext"
+import { useContext } from "react"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,12 +18,33 @@ const useStyles = makeStyles((theme) => ({
     },
     cartTitle: {
         color: "white"
+    },
+    img: {
+        borderRadius: "4px"
+    },
+    container: {
+        [theme.breakpoints.down('xs')]: {
+
+        }
+    },
+    products: {
+        borderTop: "1px solid #ddd"
+    },
+    row: {
+
+    },
+    col_left: {
+
+    },
+    thumbnail: {
+
     }
 }))
 
 export default function CartPage3() {
     const classes = useStyles()
     const cartMeta = "Nifty Wears | Cart"
+    const { cart, total, getTotal }  = useContext(CartContext)
 
     return(
         <>
@@ -34,7 +61,40 @@ export default function CartPage3() {
                         <h2>Shopping Cart</h2>
                     </Grid>
                     <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-                        
+                        <section className={cartStyles.container}>
+                            <ul className={cartStyles.products}>
+                                {cart.map(item =>{ 
+                                    return (
+                                        <li className={cartStyles.row} key={item.id}>
+                                            <div className={cartStyles.col_left}>
+                                                <div className={cartStyles.thumbnail}>
+                                                    <a href="#">
+                                                        <Image className={classes.img} src={item.photo} alt={item.title} width="100%" height="100%" />
+                                                    </a>
+                                                </div>
+                                                <div className={cartStyles.detail}>
+                                                    <div className={cartStyles.name}>
+                                                        <a>{item.title}</a>
+                                                    </div>
+                                                    <div className={cartStyles.description}>
+                                                        {item.title}
+                                                    </div>
+                                                    <div className={cartStyles.price}>
+                                                        {item.price}
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className={cartStyles.col_right}>
+                                                <div className={cartStyles.quantity}>
+                                                    
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </section>
                     </Grid>
                 </Grid>
             </Container>
