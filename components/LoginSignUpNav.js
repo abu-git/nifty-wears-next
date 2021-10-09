d50cb33cc16b3db3bdc41d8359e844b64fda089f
abu-git <abu.sammie@gmail.com>
2021-10-09 10:19:18 +0200

    signup dialog color change

--- components/LoginSignUpNav.js
+++ components/LoginSignUpNav.js
@@ -33,11 +33,19 @@ const useStyles = makeStyles((theme) => ({
             cursor: "pointer"
         }
     },
+    dialog: {
+        backgroundColor: "#f0e4af"
+    },
     pFont: {
         fontSize: "1em"
     },
     dialogTitle: {
-        border: "1px solid red",
+        //border: "1px solid red",
+        paddingBottom: "0.2em"
+    },
+    dialogContent: {
+        //border: "1px solid red",
+        //padding: "0.5em"
     }
 }))
 
@@ -61,12 +69,78 @@ export default function LoginSignUpNav() {
                 <h5 className={classes.login}>Login</h5><h5 className={classes.divider}>|</h5><h5 onClick={handleSignUpOpen} className={classes.signup}>Sign Up</h5>
             </div>
             <Dialog open={openSignUp} onClose={handleSignUpClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title" className={classes.dialogTitle}><h3>Sign Up</h3></DialogTitle>
-                <DialogContent>
+                {/*<DialogTitle id="form-dialog-title" className={classes.dialogTitle}><h3>Sign Up</h3></DialogTitle>*/}
+                <div className={classes.dialog}>
+                <DialogContent className={classes.dialogContent}>
                     <DialogContentText fontFamily="inherit">
                         <p className={classes.pFont}>Sign Up and have your delivery information safely stored with us.</p>
                     </DialogContentText>
+                    <TextField 
+                        autoFocus
+                        variant="outlined"
+                        margin="dense"
+                        id="name"
+                        label="name"
+                        type="text"
+                        fullWidth
+                    />
+                    <TextField 
+                        //autoFocus
+                        variant="outlined"
+                        margin="dense"
+                        id="phone"
+                        label="phone number"
+                        type="text"
+                        fullWidth
+                    />
+                    <TextField 
+                        //autoFocus
+                        variant="outlined"
+                        margin="dense"
+                        id="address"
+                        label="delivery address"
+                        type="text"
+                        multiline
+                        rows={3}
+                        fullWidth
+                    />
+                    <TextField 
+                        //autoFocus
+                        variant="outlined"
+                        margin="dense"
+                        id="email"
+                        label="email"
+                        type="email"
+                        fullWidth
+                    />
+                    <TextField 
+                        //autoFocus
+                        variant="outlined"
+                        margin="dense"
+                        id="password"
+                        label="password"
+                        type="password"
+                        fullWidth
+                    />
+                    <TextField 
+                        //autoFocus
+                        variant="outlined"
+                        margin="dense"
+                        id="confirmPassword"
+                        label="confirm password"
+                        type="password"
+                        fullWidth
+                    />
                 </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSignUpClose}>
+                        <h4>Cancel</h4>
+                    </Button>
+                    <Button onClick={handleSignUpClose}>
+                        <h4>SignUp</h4>
+                    </Button>
+                </DialogActions>
+                </div>
             </Dialog>
         </>
     )
