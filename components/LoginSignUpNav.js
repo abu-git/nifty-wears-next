6900142586a523c9ea769ad584ff9a3a539696e3
abu-git <abu.sammie@gmail.com>
2021-10-10 00:08:53 +0200

    login dialog added

--- components/LoginSignUpNav.js
+++ components/LoginSignUpNav.js
@@ -34,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     dialog: {
-        backgroundColor: "#f0e4af"
+        backgroundColor: "#f0e4af",
     },
     pFont: {
         fontSize: "1em"
@@ -44,8 +44,14 @@ const useStyles = makeStyles((theme) => ({
         paddingBottom: "0.2em"
     },
     dialogContent: {
+        color: "black"
         //border: "1px solid red",
         //padding: "0.5em"
+    },
+    buttonfx: {
+        backgroundColor: "#d1ba54",
+        color: "black",
+        height: "4em",
     }
 }))
 
@@ -54,19 +60,28 @@ export default function LoginSignUpNav() {
 
     //Dialog Config starts here...
     const [openSignUp, setOpenSignUp] = useState(false)
+    const [openLogin, setOpenLogin] = useState(false)
 
     const handleSignUpOpen = () => {
         setOpenSignUp(true)
+    }
+
+    const handleLoginOpen = () => {
+        setOpenLogin(true)
     }
 
     const handleSignUpClose = () => {
         setOpenSignUp(false)
     }
 
+    const handleLoginClose = () => {
+        setOpenLogin(false)
+    }
+
     return(
         <>
             <div className={classes.loginSignup}>
-                <h5 className={classes.login}>Login</h5><h5 className={classes.divider}>|</h5><h5 onClick={handleSignUpOpen} className={classes.signup}>Sign Up</h5>
+                <h5 onClick={handleLoginOpen} className={classes.login}>Login</h5><h5 className={classes.divider}>|</h5><h5 onClick={handleSignUpOpen} className={classes.signup}>Sign Up</h5>
             </div>
             <Dialog open={openSignUp} onClose={handleSignUpClose} aria-labelledby="form-dialog-title">
                 {/*<DialogTitle id="form-dialog-title" className={classes.dialogTitle}><h3>Sign Up</h3></DialogTitle>*/}
@@ -76,7 +91,7 @@ export default function LoginSignUpNav() {
                         <p className={classes.pFont}>Sign Up and have your delivery information safely stored with us.</p>
                     </DialogContentText>
                     <TextField 
-                        autoFocus
+                        //autoFocus
                         variant="outlined"
                         margin="dense"
                         id="name"
@@ -133,13 +148,49 @@ export default function LoginSignUpNav() {
                     />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleSignUpClose}>
+                    <Button onClick={handleSignUpClose} className={classes.buttonfx} variant="outlined">
                         <h4>Cancel</h4>
                     </Button>
-                    <Button onClick={handleSignUpClose}>
+                    <Button onClick={handleSignUpClose} className={classes.buttonfx} variant="outlined">
                         <h4>SignUp</h4>
                     </Button>
                 </DialogActions>
+                </div>
+            </Dialog>
+
+            <Dialog open={openLogin} onClose={handleLoginClose} aria-labelledby="form-dialog-title">
+                <div className={classes.dialog}>
+                    <DialogTitle id="form-dialog-title" className={classes.dialogTitle}>
+                        <h3>Login</h3>
+                    </DialogTitle>
+                    <DialogContent className={classes.dialogContent}>
+                        <TextField 
+                            //autoFocus
+                            variant="outlined"
+                            margin="dense"
+                            id="email"
+                            label="email"
+                            type="email"
+                            fullWidth
+                        />
+                        <TextField 
+                            //autoFocus
+                            variant="outlined"
+                            margin="dense"
+                            id="password"
+                            label="password"
+                            type="password"
+                            fullWidth
+                        />  
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleLoginClose} className={classes.buttonfx} variant="outlined">
+                            <h4>Cancel</h4>
+                        </Button>
+                        <Button onClick={handleLoginClose} className={classes.buttonfx} variant="outlined">
+                            <h4>Log In</h4>
+                        </Button>
+                    </DialogActions>
                 </div>
             </Dialog>
         </>
